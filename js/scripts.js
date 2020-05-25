f8cbf48965c57a032ef534a37c37ae2a63dcb51d
Henry Javangwe <henrysparks1@gmail.com>
2020-05-25 22:20:43 +0200

    Created a function init and then added all th ecode that we use to set up our scene inside, his we do so that we can clean upi and make our code more tidy.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
-console.log('connected and rolling...')
-// setting up the scene, the camera(this will need field of view, aspect ratio and near/far clipping planes to be defined) and the renderer (to convert our 3d image to 2d so that we can be able to see it.)
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(
-    45, 
-    window.innerWidth/window.innerHeight,
-    1,
-    1000
-);
-var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.getElementById('webGl').appendChild(renderer.domElement);
-renderer.render(
-    scene,
-    camera
-)
+function init (){
+    console.log('connected and rolling...')
+    // setting up the scene, the camera(this will need field of view, aspect ratio and near/far clipping planes to be defined) and the renderer (to convert our 3d image to 2d so that we can be able to see it.)
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(
+        45, 
+        window.innerWidth/window.innerHeight,
+        1,
+        1000
+    );
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('webGl').appendChild(renderer.domElement);
+    renderer.render(
+        scene,
+        camera
+    )
+}
+
+init();
