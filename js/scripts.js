a144e29514bf2cdd9899fce2ae67291278af8b47
Henry Javangwe <henrysparks1@gmail.com>
2020-05-25 22:30:45 +0200

    we create the shape by defining the geometry and the material then we'll combine these two to create a mesh. then added the mesh onto the scene using the scene.add(mesh methoddbad)

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,16 @@ function init (){
     console.log('connected and rolling...')
     // setting up the scene, the camera(this will need field of view, aspect ratio and near/far clipping planes to be defined) and the renderer (to convert our 3d image to 2d so that we can be able to see it.)
     var scene = new THREE.Scene();
+    var geometry = new THREE.BoxGeometry(1, 1, 1);//we create the shape by defining the geometry and the material then we'll combine these two to create a mesh.
+    var material = new THREE.MeshBasicMaterial({
+        color: 0x00ff00
+    });
+    var mesh = new THREE.Mesh(
+        geometry,
+        material
+    );
+    scene.add(mesh);
+
     var camera = new THREE.PerspectiveCamera(
         45, 
         window.innerWidth/window.innerHeight,
